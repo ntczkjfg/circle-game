014f096a9e2d1a2c48203d579b77768269952c3c
ntczkjfg <99524838+ntczkjfg@users.noreply.github.com>
2023-04-29 09:01:00 -0400

    Better pyinstaller.bat

--- pyinstaller.bat
+++ pyinstaller.bat
@@ -1,1 +1,6 @@
-C:\Users\User\AppData\Roaming\Python\Python310\Scripts\pyinstaller.exe "C:\Users\User\My Stuff\GitHub\circle-game\src\Veler.py" --onefile --noconsole --add-data "img;img"
+set appname=Veler
+C:\Users\User\AppData\Roaming\Python\Python310\Scripts\pyinstaller.exe .\src\%appname%.py --onefile --noconsole --add-data "img;img"
+rmdir /s /q .\build\
+move .\dist\%appname%.exe .
+rmdir /s /q .\dist\
+del %appname%.spec
--- src/Veler.py
+++ src/Veler.py
@@ -1,6 +1,31 @@
 from Common_Variables import *
 from Game import Game
 
+"""
+Game modes:  
+* Endless (current mode)
+* Collect all the yellows
+* Countdown timer to get all yellows
+* Countdown timer, pure survival 
+
+Powerups:  
+* Additional time (For timed modes)
+
+Work out displaying active powerups on user clearer
+Add additional game over conditions to account for game modes above
+Add menu
+Add help screen
+Better turtle icon
+Better magnet icon
+2x powerup icon
+Placeholder icon (?)
+Invulnerability needs to be displayed better
+Shield radius decreases with timer until it vanishes
+
+Fix collisions when yellow balls growing
+Fix shoving balls against walls
+"""
+
 if __name__ == '__main__':
     while True:
         newGame = Game()
